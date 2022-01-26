4c042a85138b1ff192885facaa81acefbd906836
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-25 21:28:38 -0300

    learned the first part of events DOM in Js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//console.log(document);
+//function h1Click(){
+//    console.log("Executando a função")
+//};
+
+//function changeH1(input){
+//    let h1 = document.getElementsByTagName("h1")[0]
+//    h1.innerText = input.value;       
+//}
+
+function hideh1(){
+    let h1 = document.getElementsByTagName("h1")[0]
+    h1.style.display = "none";
+}
