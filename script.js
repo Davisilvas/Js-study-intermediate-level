5ebfd09c91074e167e2a6a05c6445ba941fa602a
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-26 10:25:34 -0300

    learned new events on Js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-//function h1Click(){
-//    console.log("Executando a função")
-//};
+function h1Click(){
+    console.log("Executando a função")
+};
 
-//function changeH1(input){
-//    let h1 = document.getElementsByTagName("h1")[0]
-//    h1.innerText = input.value;       
-//}
+function changeH1(input){
+    let h1 = document.getElementsByTagName("h1")[0]
+    h1.innerText = input.value;       
+}
 
 function hideh1(){
     let h1 = document.getElementsByTagName("h1")[0]
     h1.style.display = "none";
+}
+
+function mouseOver(elemento){
+    elemento.style.backgroundColor = "red";
+
+}
+
+function mouseOut(elemento){
+    elemento.style.backgroundColor = "cornflowerblue";
 }
